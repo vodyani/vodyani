59543ba9d0e3849b8e5ba2021005a5a121cf65fb
John Denver <chogathkk@gmail.com>
2022-04-27 16:01:52 +0800

    style: code

--- src/core/dto/pagination.ts
+++ src/core/dto/pagination.ts
@@ -3,19 +3,15 @@ import { ApiProperty } from '@nestjs/swagger';
 import { PaginationQueryOptions } from '@vodyani/core';
 
 export class PaginationQueryDto implements PaginationQueryOptions {
-  @Expose()
-  @ApiProperty({ name: 'index', example: '1', type: String, description: 'index' })
+  @Expose() @ApiProperty({ name: 'index', example: '1', type: String, description: 'index' })
   public index?: string;
 
-  @Expose()
-  @ApiProperty({ name: 'size', example: '20', type: String, description: 'size' })
+  @Expose() @ApiProperty({ name: 'size', example: '20', type: String, description: 'size' })
   public size?: string;
 
-  @Expose()
-  @ApiProperty({ name: 'order_by', example: 'id', type: String, description: 'pagination order by' })
+  @Expose() @ApiProperty({ name: 'order_by', example: 'id', type: String, description: 'pagination order by' })
   public orderBy?: string;
 
-  @Expose()
-  @ApiProperty({ name: 'order_rule', example: 'desc', type: String, description: 'pagination order rule' })
+  @Expose() @ApiProperty({ name: 'order_rule', example: 'desc', type: String, description: 'pagination order rule' })
   public orderRule?: 'desc' | 'asc';
 }
--- src/core/vo/response-body.ts
+++ src/core/vo/response-body.ts
@@ -4,27 +4,21 @@ import { Expose } from '@vodyani/transformer';
 import { ResponseBody } from '../common';
 
 export class ResponseBodyVo<T> implements ResponseBody {
-  @Expose()
-  @ApiProperty({ name: 'code', type: Number, required: true, description: 'response code' })
+  @Expose() @ApiProperty({ name: 'code', type: Number, required: true, description: 'response code' })
   public code: number;
 
-  @Expose()
-  @ApiProperty({ name: 'message', type: String, required: true, description: 'response message' })
+  @Expose() @ApiProperty({ name: 'message', type: String, required: true, description: 'response message' })
   public message: string;
 
-  @Expose()
-  @ApiProperty({ name: 'request_id', type: String, required: true, description: 'request id' })
+  @Expose() @ApiProperty({ name: 'request_id', type: String, required: true, description: 'request id' })
   public requestId: string;
 
-  @Expose()
-  @ApiProperty({ name: 'request_time', type: Number, required: true, description: 'request time' })
+  @Expose() @ApiProperty({ name: 'request_time', type: Number, required: true, description: 'request time' })
   public requestTime: number;
 
-  @Expose()
-  @ApiProperty({ name: 'response_time', type: Number, required: true, description: 'response time' })
+  @Expose() @ApiProperty({ name: 'response_time', type: Number, required: true, description: 'response time' })
   public responseTime: number;
 
-  @Expose()
-  @ApiProperty({ name: 'data', type: Number, required: true, description: 'response data' })
+  @Expose() @ApiProperty({ name: 'data', type: Number, required: true, description: 'response data' })
   public data: T;
 }
