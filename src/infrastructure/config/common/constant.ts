61c2b53cabe5c83d03a06cd98adb25eae3751f0e
John Denver <chogathkk@gmail.com>
2022-09-08 12:13:33 +0800

    fix: make more reasonable adjustments to the configuration

--- src/infrastructure/config/common/constant.ts
+++
@@ -1,9 +0,0 @@
-import { toString, toNumber } from '@vodyani/utils';
-
-import { PROCESS_ENV, ENV } from './enum';
-
-export const environment = {
-  port: toNumber(process.env[PROCESS_ENV.PORT], 3000),
-  env: toString(process.env[PROCESS_ENV.ENV], ENV.LOCAL),
-  name: toString(process.env[PROCESS_ENV.NAME], 'VODYANI_SERVER'),
-};
--- src/infrastructure/config/module.ts
+++ src/infrastructure/config/module.ts
@@ -1,21 +1,26 @@
 import { ArkModule } from '@vodyani/ark';
 import { DynamicModule } from '@vodyani/core';
+import { toNumber, toString } from '@vodyani/utils';
 
-import { ENV, environment } from './common';
+import { ENV, PROCESS_ENV } from './common';
 
 import { resourcePath } from '@/core/common';
 
 export class ConfigInfrastructure {
   static forRoot(): DynamicModule {
+    const port = toNumber(process.env[PROCESS_ENV.PORT], 3000);
+    const current = toString(process.env[PROCESS_ENV.ENV], ENV.LOCAL);
+    const name = toString(process.env[PROCESS_ENV.NAME], 'VODYANI_SERVER');
+
     return {
       global: true,
       module: ConfigInfrastructure,
       imports: [
         ArkModule.forRoot({
           local: {
+            params: { port, name },
             path: `${resourcePath}/config`,
-            env: { default: ENV.DEFAULT, current: environment.env },
-            params: { port: environment.port, name: environment.name },
+            env: { default: ENV.DEFAULT, current },
           },
         }),
       ],
