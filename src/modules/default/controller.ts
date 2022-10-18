34c17936c04507418edb97cb75ac1bf0f2f4e3d8
John Denver <chogathkk@gmail.com>
2022-10-18 15:56:22 +0800

    feat: adopt easy mode without adding swagger

--- src/modules/default/controller.ts
+++ src/modules/default/controller.ts
@@ -1,11 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@vodyani/swagger';
 
 import { DefaultService } from './service';
 
-import { ApiResponseVo } from '@/core/decorator';
-
-@ApiTags('default')
 @Controller('/')
 export class DefaultController {
   constructor(
@@ -13,7 +9,6 @@ export class DefaultController {
   ) {}
 
   @Get()
-  @ApiResponseVo()
   public index() {
     return this.service.index();
   }
